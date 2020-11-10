cb8b943516d7e09df6733fb15bc82c0b1827d307
Victoria <victoria.sofianidou@gmail.com>
2020-11-10 12:29:34 +0100

    did step 3 showMyName and changeMyName with Xander's help

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -3,13 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Compiler\ResolveBindingsPass;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\Form\Forms;
 
 class LearningController extends AbstractController
 {
+
+    private string $name;
+
     /**
-     * @Route("/", name="home")
+     * LearningController constructor.
+     * @param string $name
+     */
+    public function __construct()
+    {
+        $this->name = "Unknown";
+    }
+
+    /**
+     * @Route("/learning", name="learn")
      */
     public function index(): Response
     {
@@ -36,6 +50,42 @@ class LearningController extends AbstractController
     {
 
         return $this->render('learning/aboutMe.html.twig', ['name' => 'BeCode']);
+    }
+
+/*
+showMyName: On this page you will greet the user by his name. The default name is "Unknown".
+Below the greeting there should be a form where the user can save his name.
+When submitted (POST) this should send the user to the changeMyName page.
+This page should be the homepage. If you just enter the domain name this should be the page that opens up.
+*/
+
+
+    /**
+     * @Route("/", name="showName")
+     */
+    public function showMyName(): Response
+    {
+        session_start();
+        if(isset($_SESSION["name"] )){
+
+            $this->name=$_SESSION["name"];
+
+        }
+
+        return $this->render('learning/showName.html.twig', [
+            'name' => $this ->name,
+        ]);
+    }
+
+    /**
+     * @Route("/change-name", name="changeName")
+     */
+    public function changeMyName(): Response
+    {
+
+        $_SESSION["name"]=$_POST["name"];
+        return $this->redirectToRoute('showName');
+
     }
 
 }
